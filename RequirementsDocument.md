7fc821daf4be27e1a9f2d74c300d530772dbcbc3
nevosial <nevosial@gmail.com>
2017-03-25 22:16:33 +0530

    first commit

---
+++ RequirementsDocument.md
@@ -0,0 +1,17 @@
+# Requirements Document
+
+
+### 1. User Requirements
+
+
+##### 1.1 User Characteristics  
+*The user can be anyone having access to computers and having an understanding of the English language.*  
+
+##### 1.2 System's Functionality  
+*The system will allow the user to speak (rant out) anything about his life for as long as he/she can.*
+
+##### 1.3 User Interfaces   
+*The user will be provided with a command line where he/she can start typing  aka.. ranting out !.*
+
+### 2. System Requirements  
+*Any system capable of running a Node.js web app.*
---
+++ SpeakOut.js
@@ -0,0 +1,53 @@
+//import the required
+var readline = require('readline');
+var fs = require('fs');
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+//create the object to store info.
+var Person = {
+	name: '',
+	sayings: []
+};
+
+
+rl.question("What is your name ? ", function(answer) {
+
+	Person.name = answer;
+
+	rl.setPrompt(`What would ${Person.name} say? `);
+
+	rl.prompt();
+
+	rl.on('line', function(saying) {
+
+		Person.sayings.push(saying.trim());
+
+		if (saying.toLowerCase().trim() === 'exit') {
+//        fs.writeFile("./rant.txt" , "hello world" , "UTF-8" ,  function(err){
+//          if(err) throw err;
+//          console.log("File has been saved.")
+//        });
+			rl.close();
+		} else {
+			rl.setPrompt(`What else would ${Person.name} say? ('exit' to leave) `);
+		    rl.prompt();
+        }
+
+
+	});//end the on eventListener
+
+});//end of question
+
+
+rl.on('close', function() {
+	console.log("%s is a real person that says %j", Person.name, Person.sayings);
+  /**
+  fs.writeFile('./rant.txt' , 'Hello', 'UTF-8', function(err){
+    if(err) throw err;
+    console.log("File is saved");
+  });
+  **/
+	process.exit();
+
+});
