64654a455c6fedaff83df498afd845f41b4612cf
nevosial <nevosial@gmail.com>
2017-03-27 00:50:48 +0530

    Added reading from a data file

--- expressServer.js
+++ expressServer.js
@@ -3,11 +3,27 @@
 var express = require('express');
 var app = express();
 
+//local import data from a file
+var dataFile = require('./data/data.json');
+
 var myServer = app.listen(3000 , function(){
   console.log('Express web server running at localhost:3000');
 });
 
 //In express, we do not need to worry about headers, content-type.
 app.get('/' , function(request, response){
-  response.send("<h1>Hello World</h2>");
+  var info = '';
+  dataFile.speakers.forEach(function(item){
+    //using `template string` supported in ES6.
+    info += `
+    <li>
+    <h2>${item.name}<h2>
+    <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  response.send(`
+    <h1>Renowned Speakers</h1>
+    ${info}
+    `);
 });
