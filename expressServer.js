09fd4d12bf79bd6d30da44abf1e899213e777fc1
nevosial <nevosial@gmail.com>
2017-03-27 23:34:28 +0530

    Separated the routes in a new folder and updated the serverapp

--- expressServer.js
+++ expressServer.js
@@ -6,43 +6,10 @@ var app = express();
 //local import data from a file
 var dataFile = require('./data/data.json');
 
+//Get the details of the route.
+app.use(require('./routes/index'));
+
+//creating the server.
 var myServer = app.listen(3000 , function(){
   console.log('Express web server running at localhost:3000');
-});
-
-//In express, we do not need to worry about headers, content-type.
-app.get('/' , function(request, response){
-  response.send(`
-    <h1>Renowned Speakers</h1>
-    <p>We have got together some great speakers from around the world who will help us with understanding the intricacies of the world.</p>
-    <p>lorem</p>
-    `);
-});
-
-//Adding a new route to the speakers page of the app
-app.get('/speakers' , function(request, response){
-  var info = '';
-  dataFile.speakers.forEach(function(item){
-    //using `template string` supported in ES6.
-    info += `
-    <li>
-    <h2>${item.name}<h2>
-    <p>${item.summary}</p>
-    </li>
-    `;
-  });
-  response.send(`
-    <h1>Renowned Speakers</h1>
-    ${info}
-    `);
-});
-
-//Adding a new route to get data from one speaker.
-app.get('/speakers/:speakerid' , function(request , response){
-  var spk = dataFile.speakers[request.params.speakerid];
-  response.send(`
-    <h2>${spk.name}</h2>
-    <p>${spk.title}</p>
-    <p>${spk.summary}</p>
-    `);
 });
