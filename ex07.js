b6f4d57bcbdc109036842946b5d9274564f3bcc4
nevosial <nevosial@gmail.com>
2017-04-21 00:49:40 +0530

    ex07: http client

--- ex07.js
+++ ex07.js
@@ -4,6 +4,17 @@ var client = http.get(process.argv[2], function(response){
   //console.log(response.statusCode);
   response.setEncoding('utf-8');
   response.on('data', function(d){
-    console.log(d.split('\n'));
+    console.log(d);
   });
 });
+
+
+/*
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+}).on('error', console.error)
+*/
