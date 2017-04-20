ec604ed98835f97a15cd2f54e266dbc2440b3e3c
nevosial <nevosial@gmail.com>
2017-04-21 01:19:31 +0530

    ex08: Using the bl node module

---
+++ ex08a.js
@@ -0,0 +1,16 @@
+//ex08: HTTP collect using the bl or concat-stream node modules.
+
+//Here we have used the bl node module.
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
