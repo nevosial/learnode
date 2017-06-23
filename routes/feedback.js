158e94858772ddc6e6c6ded271583044068c2484
Neville Mascarenhas <nevosial@gmail.com>
2017-06-23 13:59:10 +0530

    Update feedback.js

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,10 +1,10 @@
-//Added a new route
+//Added a new route to the app
 
 var express = require('express');
 var router = express.Router();
 
 router.get('./feedback' , function(request, response){
-  
+  //Function that describes what is served.
 
 
 });
