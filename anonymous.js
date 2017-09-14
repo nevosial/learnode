d9bccf00a4b9b8a094142b3285e1e96cfe63a662
Neville Mascarenhas <nevosial@gmail.com>
2017-09-14 14:23:11 -0700

    Anonymous functions in JS

---
+++ anonymous.js
@@ -0,0 +1,16 @@
+//Code block ex for Anonymous function in JS.
+
+var a =5;
+var b = 7;
+
+var biggest = function(){
+  var result;
+  a > b ? result = a : result = b;
+  console.log(result);
+ }
+ 
+ //invocation.
+ console.log(biggest());
+ 
+ 
+ 
